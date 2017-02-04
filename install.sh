cedccd0c90ce969485be71402a3fff0435943f26
Benjamin Herzog <mail@benchr.de>
2017-02-04 18:29:42 +0100

    Use virtualbox instead of vmware

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ brew cask install visual-studio-code
 brew cask install slack
 brew cask install bartender
 brew cask install caffeine
-brew cask install vmware-fusion
+brew cask install virtualbox
 brew cask install tower
 brew cask install sqlpro-studio
 brew cask install the-unarchiver
