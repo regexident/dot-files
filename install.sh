4fa2bd4e49a106598ebcef7b6c089588684df80f
Benjamin Herzog <mail@benchr.de>
2017-02-04 11:16:34 +0100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+brew install go
+brew install git
+brew install mercurial
+brew install openssl
+brew install loc
+brew install npm
+
+brew cask install sublime-text
+brew cask install brackets
+brew cask install visual-studio-code
+brew cask install slack
+brew cask install bartender
+brew cask install caffeine
+brew cask install vmware-fusion
+brew cask install tower
+brew cask install sqlpro-studio
+brew cask install the-unarchiver
